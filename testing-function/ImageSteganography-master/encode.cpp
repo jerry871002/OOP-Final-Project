4b2caeb66727dd306587ea3b1f277ec98863eeba
kkkkelly <39906595+kkkkelly@users.noreply.github.com>
2018-06-23 17:52:39 +0800

    加了encode new的註解

--- testing-function/ImageSteganography-master/encode.cpp
+++ testing-function/ImageSteganography-master/encode.cpp
@@ -6,13 +6,12 @@
 using namespace std;
 using namespace cv;
 
-// Checks whether the bit is set or not at a particular position.
-// Returns true if set
-// Returns false if not set
+
+//¨C¤@­Óbit¸òPos°µand¡A¥H§PÂ_¬O1ÁÙ0
 bool isBitSet(char ch, int pos) {
 	// 7 6 5 4 3 2 1 0
-	ch = ch >> pos;
-	if(ch & 1)
+	ch = ch >> pos;//Move several positions to the right
+	if(ch & 1)//ch and 1=1?
 		return true;
 	return false;
 }
@@ -20,37 +19,35 @@ bool isBitSet(char ch, int pos) {
 int main(int argc, char** argv) {
 
 	/*
-	./encode image.png textfile.txt output_image.png
+	./encode image.png textfile.txt 
 	argv[0] = ./encode
 	argv[1] = image.png
 	argv[2] = textfile.txt
-	argv[3] = output_image.png
 	*/
 
 	// Checks if proper number of arguments are passed
-	if(argc != 4) {
+	if(argc != 3) {
 		cout << "Arguments Error" << "\n";
-		exit(-1);
+		return 1;
 	}
 
 	// Stores original image
-	Mat image = imread(argv[1]);//Matæ˜¯ç”¨ä¾†å„²å­˜äºŒç‚ºçŸ©é™£çš„class
+	Mat image = imread(argv[1]);
 	if(image.empty()) {
 		cout << "Image Error\n";
-		exit(-1);
+		return 1;
 	}
 
 	// Open file for text information
 	ifstream file(argv[2]);
 	if(!file.is_open()) {
 		cout << "File Error\n";
-		exit(-1);
+		return 1;
 	}
 
-	// char to work on
+
 	char ch;
-	// reads the first char from the file
-	file.get(ch);
+	file.get(ch);//±qÀÉ®×§ì¨ú¦r¤¸¡A¥H8bitªºascii codeªí¥Ü
 	// contains information about which bit of char to work on
 	int bit_count = 0;
 	// to check whether file has ended
@@ -64,28 +61,29 @@ int main(int argc, char** argv) {
 	We are manipulating bits in such way that changing LSB of the pixel values will not make a huge difference.
 	The image will still look similiar to the naked eye.
 	*/
+	//cout<< image.rows<<"\n";
+	//cout<< image.cols<<"\n";
 
 	for(int row=0; row < image.rows; row++) {
 		for(int col=0; col < image.cols; col++) {
 			for(int color=0; color < 3; color++) {
-
 				// stores the pixel details
-				Vec3b pixel = image.at<Vec3b>(Point(row,col));
-
-				// if bit is 1 : change LSB of present color value to 1.
-				// if bit is 0 : change LSB of present color value to 0.
-				if(isBitSet(ch,7-bit_count))
-					pixel.val[color] |= 1;
+				Vec3b pixel = image.at<Vec3b>(Point(row,col));//¹³¯À
+				//veb3b¥]§t¤T­Ó¦V«× ªí¥Ü¨C­Ó¦V«×ªº¸ê®Æµ²ºc³£¬Ouchar
+				
+				
+				if(isBitSet(ch,7-bit_count))//¦pªGbit¬°1ªº¸Ü
+					pixel.val[color] |= 1;//pixel.val[color] or 1(±j¨î³]¦¨1)
 				else
-					pixel.val[color] &= ~1;
+					pixel.val[color] &= ~1;//ºû«ù­ìª¬
 
 				// update the image with the changed pixel values
 				image.at<Vec3b>(Point(row,col)) = pixel;
 
-				// increment bit_count to work on next bit
+				// increment bit_count to work on next bit()
 				bit_count++;
 
-				// if last_null_char is true and bit_count is 8, then our message is successfully encode.
+				// ¦pªGÅª¨ì¤å¦rÀÉªº³Ì«á¤@­Ó¦r¤¸ «h¸õ¥X¤T­Ó°j°é¿é¥X¹Ï¤ù
 				if(last_null_char && bit_count == 8) {
 					encoded  = true;
 					goto OUT;
@@ -106,16 +104,21 @@ int main(int argc, char** argv) {
 			}
 		}
 	}
+	
 	OUT:;
+
 
 	// whole message was not encoded
 	if(!encoded) {
 		cout << "Message too big. Try with larger image.\n";
-		exit(-1);
+		return 2;
 	}
 
-	// Writes the stegnographic image
-	imwrite(argv[3],image);
 
+	//output ¿é¥X¹Ï¤ùÀÉ¦W¥s°µ"encode+­ì¹ÏÀÉªº¦W¦r"
+	string output("encode-");
+	output =  output +argv[1];
+    imwrite(output,image);
     return 0;
 }
+
